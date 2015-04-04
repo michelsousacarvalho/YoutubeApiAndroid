fd64d48e615ca9d9b683681a9e254642f03a0004
Michel Pai <michelsousacarvalho@gmail.com>
2015-04-04 13:42:23 -0300

    Initial Commit

---
+++ app/src/main/java/app/mobwise/mobwiseapp/LyricsFragment.java
@@ -0,0 +1,45 @@
+package app.mobwise.mobwiseapp;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class LyricsFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+
+
+    public LyricsFragment() {
+        // Required empty public constructor
+    }
+
+    public static LyricsFragment newInstance(int sectionNumber) {
+        LyricsFragment fragment = new LyricsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lyrics, container, false);
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(
+                getArguments().getInt(ARG_SECTION_NUMBER));
+    }
+
+}
